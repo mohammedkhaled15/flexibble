14f452d92d8969e2daa4b136655f44a74dca35f9
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-28 20:56:43 +0200

    adding two separarte files schema and config

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -11,31 +11,3 @@ const contentful = connector.GraphQL("Contentful", {
 });
 
 g.datasource(contentful);
-// @ts-ignore
-const User = g.model("User", {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(),
-  projects: g
-    .relation(() => Project)
-    .list()
-    .optional(),
-});
-
-// @ts-ignore
-const Project = g.model("Project", {
-  title: g.string().length({ min: 3 }),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-});
-
-export default config({
-  schema: g,
-});
