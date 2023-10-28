de9ca0ee87dadd828f58a40c633aec864ca85590
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-28 21:36:06 +0200

    back to original

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,26 +1,60 @@
-import { config, connector, g } from "@grafbase/sdk";
+import { g, auth, config } from "@grafbase/sdk";
 
-const mongodb = connector.MongoDB("MongoDB", {
-  url: g.env("MONGO_ATLAS_URL"),
-  apiKey: g.env("MONGO_API_KEY"),
-  dataSource: g.env("MONGO_DATASOURCE"),
-  database: g.env("MONGO_DATABASE"),
-});
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
 
-g.datasource(mongodb);
-
-const address = g.type("Address", {
-  street: g.string().mapped("street_name"),
-});
-
-mongodb
-  .model("User", {
-    name: g.string(),
-    email: g.string().optional(),
-    address: g.ref(address),
+const post = g
+  .model("Post", {
+    title: g.string(),
+    slug: g.string().unique(),
+    content: g.string().optional(),
+    publishedAt: g.datetime().optional(),
+    comments: g
+      .relation(() => comment)
+      .optional()
+      .list()
+      .optional(),
+    likes: g.int().default(0),
+    tags: g.string().optional().list().length({ max: 5 }),
+    author: g.relation(() => user).optional(),
   })
-  .collection("users");
+  .search();
+
+const comment = g.model("Comment", {
+  post: g.relation(post),
+  body: g.string(),
+  likes: g.int().default(0),
+  author: g.relation(() => user).optional(),
+});
+
+const user = g.model("User", {
+  name: g.string(),
+  email: g.email().optional(),
+  posts: g.relation(post).optional().list(),
+  comments: g.relation(comment).optional().list(),
+
+  // Extend models with resolvers
+  // https://grafbase.com/docs/edge-gateway/resolvers
+  // gravatar: g.url().resolver('user/gravatar')
+});
 
 export default config({
   schema: g,
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
 });
