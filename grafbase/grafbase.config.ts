f9042bc2a69758b79f08c9355598406039da2e6b
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-30 15:12:45 +0200

    adding my models

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,5 @@
 import { config, connector, g } from "@grafbase/sdk";
-//@ts-ignore
+
 const mongo = connector.MongoDB("MongoDB", {
   apiKey: g.env("MONGODB_API_KEY"),
   url: g.env("MONGODB_API_URL"),
@@ -7,28 +7,25 @@ const mongo = connector.MongoDB("MongoDB", {
   database: g.env("MONGODB_DATABASE"),
 });
 
-
-
- const address = g
-   .type("Address", {
-     street: g.string(),
-     city: g.string(),
-    country: g.string(),
-   })
-
- //mongo.model("Address", {
- //  street: g.string(),
-//    city: g.string(),
-//     country: g.string(),
-//  })   .collection("addresses");
+mongo.model("Project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation("User"),
+});
 
 mongo
   .model("User", {
-    name: g.string(),
-    email: g.string(),
-    age: g.int().optional(),
-     address: g.ref(address).optional(),
-    metadata: g.json().optional(),
+    name: g.string().length({ min: 2, max: 100 }),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().length({ min: 2, max: 1000 }).optional(),
+    githubUrl: g.url().optional(),
+    linkedinUrl: g.url().optional(),
+    projects: g.ref(Project).list().optional(),
   })
   .collection("users");
 
@@ -37,4 +34,3 @@ g.datasource(mongo);
 export default config({
   schema: g,
 });
-
