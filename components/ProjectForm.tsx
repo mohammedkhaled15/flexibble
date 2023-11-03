a67a0e9857ce938b04fd6550301c22c5219e40a5
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-11-03 15:46:59 +0200

    creating usable forms

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -3,6 +3,8 @@
 import { SessionInterface } from "@/common.types"
 import Image from "next/image"
 import { ChangeEvent } from "react"
+import FormField from "./FormField"
+import { categoryFilters } from "@/constants"
 
 type ProjectFormProps = {
   type: string,
@@ -14,8 +16,14 @@ const ProjectForm = ({ type, session }: ProjectFormProps) => {
   const handleFormSubmit = (e: React.FormEvent) => { }
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => { }
 
+  const handleStateChange = (fieldName: string, value: string) => {
+
+  }
+
   const form = {
-    image: "/Screenshot 2023-11-02 144725.png"
+    image: "/Screenshot 2023-11-02 144725.png",
+    title: "",
+    description: ""
   }
 
   return (
@@ -40,6 +48,44 @@ const ProjectForm = ({ type, session }: ProjectFormProps) => {
             fill
           />
         )}
+      </div>
+
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value: string) => handleStateChange("title", value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value: string) => handleStateChange("description", value)}
+      />
+      <FormField
+        type="url"
+        title="Website Url"
+        state={form.liveSiteUrl}
+        placeholder="https://example.com"
+        setState={(value: string) => handleStateChange("liveSiteUrl", value)}
+      />
+      <FormField
+        type="url"
+        title="Github Url"
+        state={form.githubUrl}
+        placeholder="https://github.com/yourName"
+        setState={(value: string) => handleStateChange("githubUrl", value)}
+      />
+
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value: string) => handleStateChange("category", value)}
+      />
+
+      <div className="flexStart w-full">
+        <button>Create</button>
       </div>
     </form>
   )
